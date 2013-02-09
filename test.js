352e140f64dc386bfda8a8a2573277fe7414e8d6
Ezekiel Templin <zeke@templ.in>
2013-02-09 18:26:50 -0500

    Flesh out the test suite

--- test.js
+++ test.js
@@ -2,7 +2,26 @@ var test = require('tap').test
   , dymo = require("./index")
   , scale = new dymo();
 
-test('no device, no response', function (t) {
-  t.equal(scale.poll(), null);
+test('polling', function (t) {
+  t.ok(scale)
+
+  test('weight set to grams', function(t) {
+    scale.readData = function () { return [ 3, 4, 2, 0, 140, 9 ] }
+    t.similar(scale.poll(), {value:2444,unit:'grams'}, 'return weight in grams');
+    t.end();
+  });
+
+  test('weight set to ounces', function(t) {
+    scale.readData = function () { return [ 3, 4, 11, 255, 190, 3 ] }
+    t.similar(scale.poll(), {value:95.8,unit:'ounces'}, 'return weight in ounces')
+    t.end();
+  });
+
+  test('no device', function(t) {
+    scale.isAlive = function () { return false }
+    t.equal(scale.poll(), null, 'polling the scale should return nothing');
+    t.end();
+  });
+
   t.end();
 });
