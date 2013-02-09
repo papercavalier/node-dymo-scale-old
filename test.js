fb37d057e9703f0209e47a2da98911b03df3a25e
Ezekiel Templin <zeke@templ.in>
2013-02-09 18:35:56 -0500

    Assert data returned

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ test('polling', function (t) {
   test('weight set to grams', function(t) {
     scale.readData = function () { return [ 3, 4, 2, 0, 140, 9 ] }
     var data = scale.poll()
+    t.ok(data, 'return data when polled')
     t.equal(data.value, 2444, 'return correct value')
     t.equal(data.unit, 'grams', 'return correct unit')
     t.end();
@@ -16,6 +17,7 @@ test('polling', function (t) {
   test('weight set to ounces', function(t) {
     scale.readData = function () { return [ 3, 4, 11, 255, 190, 3 ] }
     var data = scale.poll()
+    t.ok(data, 'return data when polled')
     t.equal(data.value, 95.8, 'return correct value')
     t.equal(data.unit, 'ounces', 'return correct unit')
     t.end();
