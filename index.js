29ff117b5bfe9788db2142901829860747583bdc
Hakan Ensari <hakan.ensari@papercavalier.com>
2013-03-07 18:43:50 +0000

    Poll scale asynchronously

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 var DymoScale = function() {
   this.device = function() {
-    var HID = require('node-hid');
+    var HID = require("node-hid");
 
     var devices = HID.devices().filter(function(x) {
-      return x.manufacturer === 'DYMO';
+      return x.manufacturer === "DYMO";
     });
 
     if(devices.length > 0) {
       return new HID.HID(devices[0].path);
     }
-  }()
-
-  this.readData = function() {
-    if (this.device) {
-      return this.device.getFeatureReport(0xfa13, 7);
-    }
   }
 
-  this.poll = function() {
-    if (data = this.readData()) {
-      var weight = { value: 0.0, unit: false }
-      var rawValue = ((256 * data[5]) + data[4]);
+  this.read = function(cb) {
+    if (var device = this.device()) {
+      device.read(function(_, data) {
+        var weight = {value: 0.0, unit: false};
+        var raw = ((256 * data[5]) + data[4]);
 
-      if (data[1] === 4) {
-        if (data[2] === 11) {
-          weight.value = parseFloat(rawValue/10.0)
-          weight.unit = 'ounces'
-        } else if (data[2] === 2) {
-          weight.value =  rawValue
-          weight.unit =  'grams'
+        if (data[1] === 4) {
+          if (data[2] === 11) {
+            weight.value = parseFloat(raw / 10.0)
+            weight.unit = "ounces"
+          } else if (data[2] === 2) {
+            weight.value =  raw
+            weight.unit =  "grams"
+          }
         }
-      }
 
-      return weight;
+        cb(weight);
+      });
+    } else {
+      cb();
     }
   }
 }
--- test.js
+++ test.js
@@ -1,41 +1,55 @@
-var test = require('tap').test
+var test = require("tap").test
   , scale = require("./index");
 
-test('polling', function (t) {
+test("polling", function (t) {
   t.ok(scale)
 
-  test('weight set to grams', function(t) {
-    scale.readData = function () { return [ 3, 4, 2, 0, 140, 9 ] }
-    var data = scale.poll()
-    t.ok(data, 'return data when polled')
-    t.equal(data.value, 2444, 'return correct value')
-    t.equal(data.unit, 'grams', 'return correct unit')
-    t.end();
+  test("weight set to grams", function(t) {
+    scale.device = function() {
+      return { read: function (cb) { cb(null, [ 3, 4, 2, 0, 140, 9 ]); } };
+    }
+
+    scale.read(function(data) {
+      t.ok(data, "return data when polled")
+      t.equal(data.value, 2444, "return correct value")
+      t.equal(data.unit, "grams", "return correct unit")
+      t.end();
+    });
   });
 
-  test('weight set to ounces', function(t) {
-    scale.readData = function () { return [ 3, 4, 11, 255, 190, 3 ] }
-    var data = scale.poll()
-    t.ok(data, 'return data when polled')
-    t.equal(data.value, 95.8, 'return correct value')
-    t.equal(data.unit, 'ounces', 'return correct unit')
-    t.end();
+  test("weight set to ounces", function(t) {
+    scale.device = function() {
+      return { read: function (cb) { cb(null, [ 3, 4, 11, 255, 190, 3 ]); } };
+    }
+
+    scale.read(function(data) {
+      t.ok(data, "return data when polled")
+      t.equal(data.value, 95.8, "return correct value")
+      t.equal(data.unit, "ounces", "return correct unit")
+      t.end();
+    });
   });
 
-  test('weight is 0', function(t) {
-    scale.readData = function () { return [ 3, 5, 11, 255, 0, 0 ] }
-    var data = scale.poll()
-    t.ok(data, 'return data when polled')
-    t.equal(data.value, 0.0, 'return correct value')
-    t.equal(data.unit, false, 'return correct unit')
-    t.end();
+  test("weight is 0", function(t) {
+    scale.device = function() {
+      return { read: function (cb) { cb(null, [ 3, 5, 11, 255, 0, 0 ]); } };
+    }
+
+    scale.read(function(data) {
+      t.ok(data, "return data when polled")
+      t.equal(data.value, 0.0, "return correct value")
+      t.equal(data.unit, false, "return correct unit")
+      t.end();
+    });
   });
 
-  test('no device', function(t) {
-    scale.readData = function () { return false }
-    var data = scale.poll()
-    t.similar(data, null, 'polling the scale should return nothing');
-    t.end();
+  test("no device", function(t) {
+    scale.device = function() { return null; }
+
+    scale.read(function(data) {
+      t.similar(data, null, "polling the scale should return nothing");
+      t.end();
+    });
   });
 
   t.end();
