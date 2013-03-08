2997bed97c95a941536b3ae6b2609d04596e3ccb
Hakan Ensari <hakan.ensari@papercavalier.com>
2013-03-08 13:52:07 +0000

    Callback now receives error as first argument

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 var DymoScale = function() {
-  this.device = function() {
-    var HID = require("node-hid");
 
+  var HID = require("node-hid");
+
+  this.device = function() {
     var devices = HID.devices().filter(function(x) {
       return x.manufacturer === "DYMO";
     });
 
-    if(devices.length > 0) {
+    if (devices.length > 0) {
       return new HID.HID(devices[0].path);
     }
   }
 
-  this.read = function(cb) {
-    if (var device = this.device()) {
-      device.read(function(_, data) {
-        var weight = {value: 0.0, unit: false};
+  this.read = function(callback) {
+    var device = this.device();
+
+    if (device) {
+      device.read(function(error, data) {
+        if (error) {
+          return callback(error);
+        }
+
+        var weight = {value: 0, unit: null};
         var raw = ((256 * data[5]) + data[4]);
 
         if (data[1] === 4) {
           if (data[2] === 11) {
-            weight.value = parseFloat(raw / 10.0)
-            weight.unit = "ounces"
+            weight.value = parseFloat(raw / 10.0);
+            weight.unit = "ounces";
           } else if (data[2] === 2) {
-            weight.value =  raw
-            weight.unit =  "grams"
+            weight.value = raw;
+            weight.unit = "grams";
           }
         }
 
-        cb(weight);
+        callback(null, weight);
       });
     } else {
-      cb();
+      callback(new Error("device offline"));
     }
   }
+
 }
 
 module.exports = exports = new DymoScale();
--- test.js
+++ test.js
@@ -2,17 +2,17 @@ var test = require("tap").test
   , scale = require("./index");
 
 test("polling", function (t) {
-  t.ok(scale)
+  t.ok(scale);
 
   test("weight set to grams", function(t) {
     scale.device = function() {
       return { read: function (cb) { cb(null, [ 3, 4, 2, 0, 140, 9 ]); } };
     }
 
-    scale.read(function(data) {
-      t.ok(data, "return data when polled")
-      t.equal(data.value, 2444, "return correct value")
-      t.equal(data.unit, "grams", "return correct unit")
+    scale.read(function(_, data) {
+      t.ok(data, "return data");
+      t.equal(data.value, 2444, "return correct value");
+      t.equal(data.unit, "grams", "return correct unit");
       t.end();
     });
   });
@@ -22,10 +22,10 @@ test("polling", function (t) {
       return { read: function (cb) { cb(null, [ 3, 4, 11, 255, 190, 3 ]); } };
     }
 
-    scale.read(function(data) {
-      t.ok(data, "return data when polled")
-      t.equal(data.value, 95.8, "return correct value")
-      t.equal(data.unit, "ounces", "return correct unit")
+    scale.read(function(_, data) {
+      t.ok(data, "return data");
+      t.equal(data.value, 95.8, "return correct value");
+      t.equal(data.unit, "ounces", "return correct unit");
       t.end();
     });
   });
@@ -35,10 +35,10 @@ test("polling", function (t) {
       return { read: function (cb) { cb(null, [ 3, 5, 11, 255, 0, 0 ]); } };
     }
 
-    scale.read(function(data) {
-      t.ok(data, "return data when polled")
-      t.equal(data.value, 0.0, "return correct value")
-      t.equal(data.unit, false, "return correct unit")
+    scale.read(function(_, data) {
+      t.ok(data, "return data");
+      t.equal(data.value, 0.0, "return correct value");
+      t.equal(data.unit, null, "return correct unit");
       t.end();
     });
   });
@@ -46,8 +46,9 @@ test("polling", function (t) {
   test("no device", function(t) {
     scale.device = function() { return null; }
 
-    scale.read(function(data) {
-      t.similar(data, null, "polling the scale should return nothing");
+    scale.read(function(error, data) {
+      t.equal(error.message, "device offline", "return an error");
+      t.similar(data, null, "do not return data");
       t.end();
     });
   });
